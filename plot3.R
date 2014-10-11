95899a2a2fde595f89ab4941b454a67fdca85730
mgaucoin <maucoin@uwaterloo.ca>
2014-10-11 11:35:20 -0400

    *.png

--- plot3.R
+++ plot3.R
@@ -7,12 +7,13 @@ data2<-data[data$Date > "2007-01-31" & data$Date < "2007-02-03",]
 
 data2$Time<-strptime(paste(data2$Date,data2$Time), format="%Y-%m-%d %H:%M:%S")
 
+png(file="plot3.png", width=480, height =480, units="px")
 plot(data2$Time, as.numeric(data2$Sub_metering_1), xlab= "", ylab="Energy sub metering", type = "l", col="black", lwd=1)
 lines(data2$Time, as.numeric(data2$Sub_metering_2), col="red", lwd=1)
 lines(data2$Time, as.numeric(data2$Sub_metering_3),  col="blue", lwd=1)
-legend("topright",legend = c("Sub_metering_1","","Sub_metering_2","","Sub_metering_3"), bty ="y", col=c("black","white", "red","white", "blue"),lwd=1, cex=0.5)
+legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), rect, trace=TRUE, inset=0, bty ="y", col=c("black", "red", "blue"),lwd=1, pt.cex=1, cex=0.8)
+dev.off()
 
-
-dev.print(png, file="plot3.png", width=480, height =480, units="px")
+#dev.print(png, file="plot3.png", width=480, height =480, units="px") <- for whatever reason this was messing up my legend
 
 
--- plot4.R
+++ plot4.R
@@ -4,6 +4,7 @@ data$Date<-as.Date(data$Date, format="%d/%m/%Y")
 data2<-data[data$Date > "2007-01-31" & data$Date < "2007-02-03",]
 data2$Time<-strptime(paste(data2$Date,data2$Time), format="%Y-%m-%d %H:%M:%S")
 
+png(file="plot4.png", width=480, height =480, units="px")
 par(mfrow=c(2,2))
 
 plot(data2$Time, as.numeric(data2$Global_active_power), xlab= "", ylab="Global Active Power", type = "l")
@@ -13,10 +14,10 @@ plot(data2$Time, as.numeric(data2$Voltage), xlab= "datetime", ylab="Voltage", ty
 plot(data2$Time, as.numeric(data2$Sub_metering_1), xlab= "", ylab="Energy sub metering", type = "l", col="black", lwd=1)
 lines(data2$Time, as.numeric(data2$Sub_metering_2), col="red", lwd=1)
 lines(data2$Time, as.numeric(data2$Sub_metering_3),  col="blue", lwd=1)
-legend("top",legend = c("Sub_metering_1","","Sub_metering_2","","Sub_metering_3"), bty ="n", col=c("black","white", "red","white", "blue"),lwd=1, cex=0.8)
+legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"), bty ="n", col=c("black","red", "blue"),lwd=1, cex=0.8)
 
 plot(data2$Time, as.numeric(data2$Global_reactive_power), xlab= "datetime", ylab="Global_reactive_power", type = "l")
-
-dev.print(png, file="plot4.png", width=480, height =480, units="px")
+dev.off()
+#dev.print(png, file="plot4.png", width=480, height =480, units="px")
 
 
